5e5a1ec935606bf325754c15b580a4a25612fca5
joaoDouglass2 <joaodouglasz12@gmail.com>
2025-06-21 05:37:44 -0300

    first commit

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCkHvFhzw8h7PsvhX9DShT10snbw15FzrE",
@@ -11,4 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
+export const auth = getAuth(app);
